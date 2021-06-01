1b09d51e6f3bccc8e44e514e15156665e6794652
Adam Mohammed <13.adamm@gmail.com>
2021-06-01 12:37:36 -0400

    added variables and sums

---
+++ variables.php
@@ -0,0 +1,14 @@
+<?php
+
+  $name = "Adam";
+  $age = 29;
+
+  echo "Hello, $name. You are $age years old.\n";
+
+  $x = 195793;
+  $y = 256836;
+  $sum = $x + $y;
+
+  echo "The sum of $x and $y is $sum."
+
+?>
